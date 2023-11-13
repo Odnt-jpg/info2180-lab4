5bf97da3eea84772ac680857e409957ab9ef4323
Odnt <remyalexd2003@gmail.com>
2023-11-12 20:21:18 -0500

    exercise 3 commit

--- app.js
+++ app.js
@@ -1,46 +1,40 @@
 "use strict";
-document.addEventListener("DOMContentLoaded", function(){
+
+document.addEventListener("DOMContentLoaded", function () {
     var searchbtn = document.getElementById("search-btn");
     var searchtext = document.getElementById("search-input");
-    var results = document.getElementById("result");
+    var result = document.getElementById("result");
 
-    searchbtn.addEventListener("click", function()
-    {var xhr = new XMLHttpRequest();
+    searchbtn.addEventListener("click", function () {
+        var xhr = new XMLHttpRequest();
 
-        if (searchtext.value !== "")
-            {xhr.onreadystatechange = function() {
-                console.log(xhr.responseText)
+        if (searchtext.value !== "") {
+            xhr.onreadystatechange = function () {
                 if (xhr.readyState == 4 && xhr.status == 200) {
-                    displayResult(xhr.responseText);;
-                    console.log(xhr.responseText)
+                    displayResult(xhr.responseText);
                 }
-            };}
-        else{
+            };
+        } else {
             alert("Please type a name");
         }
-        
-    var url = "superheroes.php?query="+ encodeURIComponent(searchtext.value.trim());
-    xhr.open("GET", url, true);
-    xhr.send();
-})
 
-    
-
+        var url = "superheroes.php?query=" + encodeURIComponent(searchtext.value.trim());
+        xhr.open("GET", url, true);
+        xhr.send();
+    });
 
     function displayResult(response) {
-        alert(response);
         var data = JSON.parse(response);
         result.innerHTML = "";
 
         if (Array.isArray(data) && data.length > 0) {
-                var hero = data[0];
-                result.innerHTML += "<h3>" + hero.alias + "</h3>";
-                result.innerHTML += "<h4>" + hero.name + "</h4>";
-                result.innerHTML += "<p>" + hero.biography + "</p>";
-            
+            var hero = data[0];
+            result.innerHTML += '<img src="' + hero.img + '">';
+            result.innerHTML += "<h3>" + hero.alias + "</h3>";
+            result.innerHTML += "<h4>" + hero.name + "</h4>";
+            result.innerHTML += "<p>" + hero.biography + "</p>";
         } else {
             result.innerHTML = "Superhero not found";
         }
     }
-
-})
+});
