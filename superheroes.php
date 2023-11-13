35c950a221aaec75d1e8ef18328520bdffd283d6
Odnt <remyalexd2003@gmail.com>
2023-11-12 23:16:22 -0500

    Formatted website, added pictures

--- superheroes.php
+++ superheroes.php
@@ -5,70 +5,70 @@ $superheroes = [
       "id" => 1,
       "name" => "Steve Rogers",
       "alias" => "Captain America",
-      "img" => "Cap.jpg",
+      "img" => "cap.jpg",
       "biography" => "Recipient of the Super-Soldier serum, World War II hero Steve Rogers fights for American ideals as one of the world’s mightiest heroes and the leader of the Avengers.",
   ],
   [
       "id" => 2,
       "name" => "Tony Stark",
       "alias" => "Ironman",
-      "img" => "Cap.jpg",
+      "img" => "tony.jpg",
       "biography" => "Genius. Billionaire. Playboy. Philanthropist. Tony Stark's confidence is only matched by his high-flying abilities as the hero called Iron Man.",
   ],
   [
       "id" => 3,
       "name" => "Peter Parker",
       "alias" => "Spiderman",
-      "img" => "spiderman.jpg",
+      "img" => "spiderman.png",
       "biography" => "Bitten by a radioactive spider, Peter Parker’s arachnid abilities give him amazing powers he uses to help others, while his personal life continues to offer plenty of obstacles.",
   ],
   [
       "id" => 4,
       "name" => "Carol Danvers",
       "alias" => "Captain Marvel",
-      "img" => "Cap.jpg",
+      "img" => "marv.jpg",
       "biography" => "Carol Danvers becomes one of the universe's most powerful heroes when Earth is caught in the middle of a galactic war between two alien races.",
   ],
   [
       "id" => 5,
       "name" => "Natasha Romanov",
       "alias" => "Black Widow",
-      "img" => "Cap.jpg",
+      "img" => "bw.jpg",
       "biography" => "Despite super spy Natasha Romanoff’s checkered past, she’s become one of S.H.I.E.L.D.’s most deadly assassins and a frequent member of the Avengers.",
   ],
   [
       "id" => 6,
       "name" => "Bruce Banner",
       "alias" => "Hulk",
-      "img" => "Cap.jpg",
+      "img" => "hulk.jpg",
       "biography" => "Dr. Bruce Banner lives a life caught between the soft-spoken scientist he’s always been and the uncontrollable green monster powered by his rage.",
   ],
   [
       "id" => 7,
       "name" => "Clint Barton",
       "alias" => "Hawkeye",
-      "img" => "Cap.jpg",
+      "img" => "hawk.jpg",
       "biography" => "A master marksman and longtime friend of Black Widow, Clint Barton serves as the Avengers’ amazing archer.",
   ],
   [
       "id" => 8,
       "name" => "T'challa",
       "alias" => "Black Panther",
-      "img" => "Cap.jpg",
+      "img" => "tch.jpg",
       "biography" => "T’Challa is the king of the secretive and highly advanced African nation of Wakanda - as well as the powerful warrior known as the Black Panther.",
   ],
   [
       "id" => 9,
       "name" => "Thor Odinson",
       "alias" => "Thor",
-      "img" => "Cap.jpg",
+      "img" => "thor.jpg",
       "biography" => "The son of Odin uses his mighty abilities as the God of Thunder to protect his home Asgard and planet Earth alike.",
   ],
   [
       "id" => 10,
       "name" => "Wanda Maximoff",
       "alias" => "Scarlett Witch",
-      "img" => "Cap.jpg",
+      "img" => "scar.jpg",
       "biography" => "Notably powerful, Wanda Maximoff has fought both against and with the Avengers, attempting to hone her abilities and do what she believes is right to help the world.",
   ], 
 ];
@@ -82,17 +82,18 @@ foreach ($superheroes as $hero) {
     }
     
 }
-if (empty($foundHeroes)) {
-    $foundHeroes[] = '<ul>';
-    foreach ($superheroes as $superhero) {
-        $foundHeroes[] = '<li>' . $superhero['alias'] . '</li>';
+if (empty($searchQuery)) {
+     foreach ($superheroes as $hero) {
+        echo ('<li>' . $hero['alias'] . '</li>');
     }
-    $foundHeroes[] = '</ul>';
+
+}else{
+    echo json_encode($foundHeroes);
 }
 
 
 header('Content-Type: application/json');
-echo json_encode($foundHeroes);
+
 ?>
 
 
